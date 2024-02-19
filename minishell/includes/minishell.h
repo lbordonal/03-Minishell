248421d265566954a2c1a8e907455874b181e162
lbordonal <lbordonal@gmail.com>
2024-02-19 23:34:50 +0000

    19/02/24 - 23:34

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 22:51:39 by lbordona          #+#    #+#             */
+/*   Updated: 2024/02/19 23:33:15 by lbordona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+/* Includes: */
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <strings.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <fcntl.h>
+# include <limits.h>
+/* Defines: */
+
+/* Structures: */
+typedef struct s_minishell
+{
+	//estrutura para o minisheel a ser criada
+}	t_minishell;
+
+/* Functions: */
+
+#endif
--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,18 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 19:45:51 by lbordona          #+#    #+#             */
-/*   Updated: 2024/02/19 19:45:53 by lbordona         ###   ########.fr       */
+/*   Updated: 2024/02/19 23:34:14 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	if (ac > 1)
+	{
+		printf("Error → Entry should be: ./minishell ");
+		printf("and shouldn't contain any arguments.\n");
+	}
+}
