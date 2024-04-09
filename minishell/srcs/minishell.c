e03289ccd57590c572637d322d8509edec55184c
Goncalo Da Silva Branco Pimenta <goda-sil@c2r4s8.42porto.com>
2024-04-10 00:17:45 +0100

    run every token

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: goda-sil <goda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 19:45:51 by lbordona          #+#    #+#             */
-/*   Updated: 2024/04/09 23:28:28 by goda-sil         ###   ########.fr       */
+/*   Updated: 2024/04/09 23:35:14 by goda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ void	door(t_list *token, char *temp, t_minishell *cmd)
 
 	args = token;
 	add_history(temp);
-	if (ft_strcmp(((t_token *)(args->content))->args[0], "echo") == 0)
-		echo_cmd(((t_token *)(args->content))->args);
-	else if (ft_strcmp(((t_token *)(args->content))->args[0], "pwd") == 0)
-		pwd_cmd(cmd);
-	else if (ft_strcmp(((t_token *)(args->content))->args[0], "cd") == 0)
-		cd_cmd(((t_token *)(args->content))->args[1]);
-	else if (ft_strcmp(((t_token *)(args->content))->args[0], "env") == 0)
-		print_list_env(cmd->env);
-	else if (ft_strcmp(((t_token *)(args->content))->args[0], "exit") == 0)
-		exit_cmd(cmd);
+	while (args != NULL)
+	{
+		if (ft_strcmp(((t_token *)(args->content))->args[0], "echo") == 0)
+			echo_cmd(((t_token *)(args->content))->args);
+		else if (ft_strcmp(((t_token *)(args->content))->args[0], "pwd") == 0)
+			pwd_cmd(cmd);
+		else if (ft_strcmp(((t_token *)(args->content))->args[0], "cd") == 0)
+			cd_cmd(((t_token *)(args->content))->args[1]);
+		else if (ft_strcmp(((t_token *)(args->content))->args[0], "env") == 0)
+			print_list_env(cmd->env);
+		else if (ft_strcmp(((t_token *)(args->content))->args[0], "exit") == 0)
+			exit_cmd(cmd);
+		args = args->next;
+	}
 }
 
 void	init_fun(t_minishell	*cmd, char **sys_env)
