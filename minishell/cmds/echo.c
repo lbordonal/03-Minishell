e521dfd9c6ff8bc33f4fc379d6cb88be829001b6
Lucas Bordonal <lbordonal@gmail.com>
2024-04-14 13:11:48 +0100

    14/04/24 - 13:11 - alterações no echo

--- minishell/cmds/echo.c
+++ minishell/cmds/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: goda-sil <goda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 11:04:34 by lbordona          #+#    #+#             */
-/*   Updated: 2024/04/09 22:56:15 by goda-sil         ###   ########.fr       */
+/*   Updated: 2024/04/14 13:10:27 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,55 @@ void	echo_cmd(char **string)
 	int	counter;
 
 	counter = 0;
+	verify_quote(string[counter]);
 	while (string[++counter])
 	{
 		if (counter > 1)
 			printf(" ");
-		printf("%s", string[counter]);
+		if (verify_quote(string[counter]))
+			printf("%s", remove_quote(string[counter]));
+		else
+			printf("%s", "ERROR-> faltou fechar aspas nessa quote");
 	}
 	printf("\n");
+}
+
+int	verify_quote(char *str)
+{
+	int	i;
+	int	quote;
+
+	i = 0;
+	quote = 2;
+	//printf("%s", str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '"')
+			quote++;
+		i++;
+	}
+	if (quote % 2 == 0)
+		return (1);
+	return (0);
+}
+
+char	*remove_quote(char *str)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+
+	i = 0;
+	j = 0;
+	new_str = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	while (str[i] != '\0')
+	{
+		if (str[i] == '"')
+			i++;
+		new_str[j] = str[i];
+		i++;
+		j++;
+	}
+	printf("%s", new_str);
+	return (new_str);
 }
--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: goda-sil <goda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 22:51:39 by lbordona          #+#    #+#             */
-/*   Updated: 2024/04/09 23:27:14 by goda-sil         ###   ########.fr       */
+/*   Updated: 2024/04/14 12:58:12 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ char	**ft_split_new(char const *s, char c);
 /* srcs: */
 void	return_string(char *cmd);
 void	echo_cmd(char **string);
+int		verify_quote(char *str);
+char	*remove_quote(char *str);
 void	cd_cmd(char *string);
 void	pwd_cmd(t_minishell *cmd);
 void	exit_cmd(t_minishell *cmd);
